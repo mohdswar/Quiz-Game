9fc1d62e16d11fa4e774ced3907497cd1137a9c4
mohdswar9@gmail.com <mohdswar9@gmail.com>
2025-01-19 09:26:18 +0300

    working hard on html css

---
+++ app.js
@@ -0,0 +1,51 @@
+/*-------------- Constants -------------*/
+const questions = [
+    {
+        question: "whos the best one in the class",
+        answers: [
+            { text: "anyone", correct: false },
+            { text: "mohammed", correct: true },
+            { text: "someone", correct: false },
+            { text: "idk", correct: false }
+        ]
+    },
+    {
+        question: "whos the best one in the class",
+        answers: [
+            { text: "anyone", correct: false },
+            { text: "mohammed", correct: true },
+            { text: "someone", correct: false },
+            { text: "idk", correct: false }
+        ]
+    },
+    {
+        question: "whos the best one in the class",
+        answers: [
+            { text: "anyone", correct: false },
+            { text: "mohammed", correct: true },
+            { text: "someone", correct: false },
+            { text: "idk", correct: false }
+        ]
+    },
+    {
+        question: "whos the best one in the class",
+        answers: [
+            { text: "anyone", correct: false },
+            { text: "mohammed", correct: true },
+            { text: "someone", correct: false },
+            { text: "idk", correct: false }
+        ]
+    },
+];
+
+/*---------- Variables (state) ---------*/
+
+
+/*----- Cached Element References  -----*/
+
+
+/*-------------- Functions -------------*/
+
+
+/*----------- Event Listeners ----------*/
+
